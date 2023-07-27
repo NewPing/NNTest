dd158646ea823f16bae88c42f82bd4e67c77294f
Newping <Jan-roth1998@web.de>
2023-07-27 21:33:24 +0200

    .

--- NeuroTesting/Program.cs
+++ NeuroTesting/Program.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Diagnostics;
 using System.Windows.Forms;
+using static Tensorflow.Binding;
+using Tensorflow.NumPy;
+using Tensorflow;
 
 namespace NeuroTesting
 {
@@ -7,7 +11,25 @@ namespace NeuroTesting
     {
         static void Main()
         {
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+
+            // Create a tensor holds a scalar value
+            var t1 = new Tensor(3);
+
+            // Init from a string
+            var t2 = new Tensor("Hello! TensorFlow.NET");
+
+            // Tensor holds a ndarray
+            var nd = new NDArray(new int[] { 3, 1, 1, 2 });
+            var t3 = new Tensor(nd);
+
+            var c1 = tf.constant(3); // int
+            var c2 = tf.constant(1.0f); // float
+            var c3 = tf.constant(2.0); // double
+            var c4 = tf.constant("Big Tree"); // string
+
+            Debug.WriteLine($"t1: {t1}, t2: {t2}, t3: {t3}");
+            Debug.WriteLine($"c1: {c1}, c2: {c2}, c3: {c3}, c4: {c4} ");
         }
     }
 }
